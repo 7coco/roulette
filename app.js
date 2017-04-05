f6efb8733b6da17547e0bd617a20b8a8a60245df
7coco <coco.greenery@gmail.com>
2017-04-05 15:24:26 +0900

    ごめん。動くところまで書いたから一気にコミットします。

---
+++ app.js
@@ -0,0 +1,53 @@
+$(function(){
+  var $startButton = $("#startButton");
+  var $theme = $("#theme");
+  var $stopButton = $("#stopButton");
+
+  var themes = ["お題1", "お題2", "お題3", "お題4", "お題5", "お題6"];
+  var hundredInterval;
+
+  $startButton.click(function(){
+    $stopButton.css("display", "block");
+    customInterval = setInterval(function(){
+      $theme.text(themes[Math.floor(Math.random() * themes.length)]);
+    }, 30);
+  });
+
+  $("#stopButton").click(function(){
+    $stopButton.css("display", "none");
+    setTimeout(function(){
+      customSetInterval(100);
+      setTimeout(function(){
+        customSetInterval(200);
+        setTimeout(function(){
+          customSetInterval(400);
+          setTimeout(function(){
+            customSetInterval(600);
+            clearInterval(customInterval);
+            themeDecideAnimation();
+          }, 2000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  });
+
+  function customSetInterval(ms){
+    clearInterval(customInterval);
+    customInterval = setInterval(function(){
+      $theme.text(themes[Math.floor(Math.random() * themes.length)]);
+    }, ms);
+  }
+
+  function customClearIntervalAfter(ms){
+    setTimeout(function(){
+      clearInterval(customInterval);
+      customSetInterval()
+    }, ms);
+  }
+
+  function themeDecideAnimation(){
+    $theme.animate( { width:"500px" }, { queue:false, duration:3000 } )
+                  .animate( { fontSize:"4em" }, 1500 )
+                  .animate( { borderRightWidth:"15px" }, 1500);
+  }
+});
